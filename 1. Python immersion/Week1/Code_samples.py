5023c2614a70d61190dc45efb364e193c5530df4
Nikita Krasnov <kras.nikita.nov@gmail.com>
2021-06-30 00:21:28 +0300

    base types

--- 1. Python immersion/Week1/Code_samples.py
+++ 1. Python immersion/Week1/Code_samples.py
@@ -5,3 +5,33 @@ print(num)
 
 num = 100_000_000 # possible to divide
 print(num)
+
+### Function type
+
+num = 123
+print(type(num))
+
+x = 1.5e2 ## exponential representation
+
+## conversion OK
+num = 1250.2
+num = int(num)
+num = float(num)
+
+# complex numbers
+# decimal + fractions modules -->
+
+print(100%1)
+
+
+#### 1. Euclidean distance ( OK )
+x, y = 0, 0
+## mutable VS immutable
+y = []
+x = y
+x.append(1)
+print(x)
+print(y)
+
+
+
